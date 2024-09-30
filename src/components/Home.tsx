e3e7c0dc67a706ab19102df33632573363b152ff
Jo <johj703@nate.com>
2024-09-30 21:50:26 +0900

    add: Home 컴포넌트 생성

---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>League Of Legend Info에 오신 것을 환영합니다!</h1>
+      <p>
+        위의 메뉴를 통해 챔피언, 아이템, 로테이션 정보를 찾아볼 수 있습니다.
+      </p>
+    </div>
+  );
+}
