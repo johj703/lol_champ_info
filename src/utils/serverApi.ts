7047c86d679aa8f65b5e4606e45268214c83f8ce
Jo <johj703@nate.com>
2024-10-01 23:11:23 +0900

    fix: 챔피언 목록을 가지고 오는 fetchChampionList 작성

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -9,3 +9,15 @@ export const fetchVersion = async (): Promise<string> => {
   // 최신 버전 반환
   return versions[0];
 };
+
+export const fetchChampionList = async (): Promise<any> => {
+  const version = await fetchVersion();
+  const response = await fetch(
+    `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion.json`
+  );
+  if (!response.ok) {
+    throw new Error("챔피언 목록을 가져오는 데 실패했습니다.");
+  }
+  const data = await response.json();
+  return data.data;
+};
