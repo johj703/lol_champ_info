980e0469320d17fb87710a11b37e6d317f77d494
Jo <johj703@nate.com>
2024-10-01 23:46:01 +0900

    add: 챔피언 상세 정보를 보여주는 ChampionDetail 페이지 작성

---
+++ src/app/champions/[championId]/page.tsx
@@ -0,0 +1,12 @@
+import { fetchChampionDetail } from "@/utils/serverApi";
+
+export default async function ChampionDetailPage({ params }: Props) {
+  const championDetail = await fetchChampionDetail(params.championId);
+  return (
+    <>
+      <h1>{championDetail.name}</h1>
+      <p>{championDetail.title}</p>
+      <p>{championDetail.blurb}</p>
+    </>
+  );
+}
