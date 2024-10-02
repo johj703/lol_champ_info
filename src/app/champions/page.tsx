d214d6113ddf85233b64f36b7d2e8c194b6a0284
Jo <johj703@nate.com>
2024-10-02 23:17:02 +0900

    fix: ISR 렌더링 방식을 사용하기 위해 revalidate 시간을 1시간으로 정해서 1시간마다 정적으로 렌더링 되도록 수정

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,47 +1,31 @@
-import { Champion, fetchChampionList } from "@/utils/serverApi";
+import { fetchChampionList } from "@/utils/serverApi";
+import { Champion } from "@/types/Champion";
 import Image from "next/image";
-import Link from "next/link";
 
-interface ChampionListPageProps {
-  champions: Champion[];
-}
+// ISR 렌더링 방식으로 하우레 한 번 페이지를 재검증하도록 설정
+export const revalidate = 86400;
 
-const ChampionListPage: React.FC<ChampionListPageProps> = ({ champions }) => {
+const ChampionListPage = async () => {
+  // 챔피언 목록 데이터를 가지고 오기
+  const champions: Champion[] = await fetchChampionList();
   return (
     <div>
       <h1>챔피언 목록</h1>
-      <div>
+      <ul className="champion-list">
         {champions.map((champion) => (
-          <Link key={champion.id} href={`/champions/${champion.id}`}>
-            <a>
-              <div>
-                <Image
-                  src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg`}
-                  alt={champion.name}
-                  width={120}
-                  height={120}
-                />
-                <h2>{champion.name}</h2>
-              </div>
-            </a>
-          </Link>
+          <div key={champion.id} className="champion-card">
+            <Image
+              src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`}
+              alt={champion.name}
+              width={120}
+              height={120}
+            />
+            <p>{champion.name}</p>
+          </div>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
 
 export default ChampionListPage;
-
-// ISR을 사용해서 데이터 가지고 오기
-export async function getStaticProps() {
-  const champions = await fetchChampionList();
-
-  return {
-    props: {
-      champions,
-    },
-    // 하루에 한 번씩(86400초) 재검증하도록 revalidate를 86400초로 설정
-    revalidate: 86400,
-  };
-}
