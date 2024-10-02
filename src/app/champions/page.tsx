6b6ed0c44dee41e31735531cbd2d9631a33f4ccd
Jo <johj703@nate.com>
2024-10-02 17:48:02 +0900

    fix: 챔피언데이터는 자주 변경되지 않으므로 ISR을 사용해서 챔피언 목록 페이지 작성

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,24 +1,34 @@
 import { Champion, fetchChampionList } from "@/utils/serverApi";
+import Image from "next/image";
+import Link from "next/link";
 
 interface ChampionListPageProps {
   champions: Champion[];
 }
 
-export default async function ChampionsPage() {
-  const championList = await fetchChampionList();
-
+const ChampionListPage: React.FC<ChampionListPageProps> = ({ champions }) => {
   return (
-    <>
+    <div>
       <h1>챔피언 목록</h1>
-      <ul>
-        {Object.keys(championList).map((championId) => (
-          <li key={championId}>
-            <a href={`/champions/${championId}`}>
-              {championList[championId].name}
+      <div>
+        {champions.map((champion) => (
+          <Link key={champion.id} href={`/champions/${champion.id}`}>
+            <a>
+              <div>
+                <Image
+                  src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg`}
+                  alt={champion.name}
+                  width={120}
+                  height={120}
+                />
+                <h2>{champion.name}</h2>
+              </div>
             </a>
-          </li>
+          </Link>
         ))}
-      </ul>
-    </>
+      </div>
+    </div>
   );
-}
+};
+
+export default ChampionListPage;
