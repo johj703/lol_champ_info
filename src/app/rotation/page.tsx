0303393b02f2a2f3fac035f0c14a356d96460145
Jo <johj703@nate.com>
2024-10-05 18:39:21 +0900

    fix: useEffect를 사용해서 챔피언 로테이션 API를 불러올 fetchData 함수 작성

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import { getChampionRotation } from "@/utils/serverApi";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import { useState } from "react";
 
 interface Champion {
   id: number;
@@ -12,6 +13,22 @@ const RotationPage = () => {
   const [champions, setChampions] = useState<Champion[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getChampionRotation();
+        // API의 챔피언 리스트 형식에 맞게 수정
+        setChampions(data.freeChampionIds);
+        setLoading(false);
+      } catch (error) {
+        setError("챔피언 로테이션 데이터를 가져오는 중 오류가 발생했습니다.");
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div>
