5895806d15e592a3b218e074f1fbcbf6fa66036d
Jo <johj703@nate.com>
2024-10-05 18:34:31 +0900

    fix: 챔피언 로테이션을 보여지도록 작성

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,7 +1,34 @@
 "use client";
 
+import Image from "next/image";
+import { useState } from "react";
+
+interface Champion {
+  id: number;
+  name: string;
+}
+
 const RotationPage = () => {
-  return <div>page</div>;
+  const [champions, setChampions] = useState<Champion[]>([]);
+
+  return (
+    <div>
+      <h1>챔피언 로테이션</h1>
+      <ul>
+        {champions.map((champion) => (
+          <li key={champion.id}>
+            <Image
+              src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`}
+              alt={champion.name}
+              width={120}
+              height={120}
+            />
+            <p>{champion.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default RotationPage;
