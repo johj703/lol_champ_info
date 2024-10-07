476a6bdefc86aa4a33022d998db9edd1677f534c
Jo <johj703@nate.com>
2024-10-07 23:17:01 +0900

    fix: Home페이지 스타일 추가

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <div>
-      <h1>League Of Legend Info에 오신 것을 환영합니다!</h1>
-      <p>
+    <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center p-6">
+      <h1 className="text-4xl font-bold">
+        League Of Legend Info에 오신 것을 환영합니다!
+      </h1>
+      <p className="text-lg text-gray-300 text-center">
         위의 메뉴를 통해 챔피언, 아이템, 로테이션 정보를 찾아볼 수 있습니다.
       </p>
     </div>
