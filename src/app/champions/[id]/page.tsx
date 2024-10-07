bcaffb60a4f9a5a2c230823204675af7fb281ed6
Jo <johj703@nate.com>
2024-10-07 22:09:58 +0900

    fix: Champion 상세 페이지 스타일 추가

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -11,39 +11,43 @@ interface ChampionDetailPageProps {
 const ChampionDetailPage = async ({ params }: ChampionDetailPageProps) => {
   const champion: ChampionDetail = await fetchChampionDetail(params.id);
   return (
-    <div>
-      <h1>{champion.name}</h1>
-      <h2>{champion.title}</h2>
+    <div className="bg-gray-900 text-gray-200 p-4 rounded-lg shadow-lg">
+      <h1 className="text-red-500 text-2xl font-bold">{champion.name}</h1>
+      <h2 className="text-gray-400 font-semibold">{champion.title}</h2>
       <Image
         src={`https://ddragon.leagueoflegends.com/cdn/12.22.1/img/champion/${champion.image}`}
         alt={champion.name}
         width={400}
         height={400}
-        className="rounded-lg mb-4"
+        className="rounded-lg mb-4 border-2 border-none"
       />
 
-      <p>{champion.blurb}</p>
+      <p className="text-blue-900">{champion.blurb}</p>
 
       {/* 추가 정보 렌더링 */}
-      <section>
-        <h3>Lore</h3>
+      <section className="text-gray-200 mt-4">
+        <h3 className="text-yellow-500 font-semibold">Lore</h3>
         <p>{champion.lore}</p>
 
-        <h3>Ally Tips</h3>
+        <h3 className="text-yellow-500 font-semibold">Ally Tips</h3>
         <ul>
           {champion.allytips.map((tip, index) => (
-            <li key={index}>{tip}</li>
+            <li key={index} className="list-disc list-inside ml-4">
+              {tip}
+            </li>
           ))}
         </ul>
 
-        <h3>Enemy Tips</h3>
+        <h3 className="text-yellow-500 font-semibold">Enemy Tips</h3>
         <ul>
           {champion.enemytips.map((tip, index) => (
-            <li key={index}>{tip}</li>
+            <li key={index} className="list-disc list-inside ml-4">
+              {tip}
+            </li>
           ))}
         </ul>
 
-        <h3>Tags</h3>
+        <h3 className="text-yellow-500 font-semibold">Tags</h3>
         <p>{champion.tags.join(", ")}</p>
       </section>
     </div>
