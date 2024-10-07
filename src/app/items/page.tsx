2eb7c7df52180a2e8c4e5501ff04c5aaaa9f7b21
Jo <johj703@nate.com>
2024-10-07 18:43:29 +0900

    fix: items 페이지에 item들 버전과 이미지 출력할 Image 컴포넌트 추가

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,17 +1,30 @@
-import { fetchItemList, Item } from "../../utils/serverApi";
+import Image from "next/image";
+import { fetchItemList } from "../../utils/serverApi";
+import { Item } from "@/types/Item";
 
 const ItemPage = async () => {
   // 서버에서 데이터 가지고 오기
-  const items: Item[] = await fetchItemList();
+  const result = await fetchItemList();
+
+  const version = result.versions;
+  const items: Item[] = result.items;
+  console.log("items", items);
   return (
     <>
       <h1>아이템 목록</h1>
       <ul>
         {items.map((item) => (
           <li key={item.id}>
+            <Image
+              src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${item.image.full}`}
+              alt={item.name}
+              width={200}
+              height={100}
+            />
             <h2>{item.name}</h2>
-            <p>{item.plaintext}</p>
             <p>가격: {item.gold.total}골드</p>
+            <p>팔 때: {item.gold.sell}골드</p>
+            <p>{item.plaintext}</p>
           </li>
         ))}
       </ul>
