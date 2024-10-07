2f64a129361f4443ad75c7b1c7ae652035bc12e9
Jo <johj703@nate.com>
2024-10-07 22:07:53 +0900

    fix: NavBar를 깔끔한 색상으로 변경, 네비게이션 목록 정렬 수정

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,20 +2,22 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800 text-white p-4">
-      <ul className="container mx-auto flex space-x-4">
+    <nav className="bg-blue-900 text-white">
+      <ul className="container mx-auto flex justify-between items-center py-4">
         <li>
           <Link href="/">홈</Link>
         </li>
-        <li>
-          <Link href="/champions">챔피언 목록</Link>
-        </li>
-        <li>
-          <Link href="/items">아이템 목록</Link>
-        </li>
-        <li>
-          <Link href="/rotation">로테이션 정보</Link>
-        </li>
+        <div className="flex space-x-10">
+          <li>
+            <Link href="/champions">챔피언 목록</Link>
+          </li>
+          <li>
+            <Link href="/items">아이템 목록</Link>
+          </li>
+          <li>
+            <Link href="/rotation">로테이션 정보</Link>
+          </li>
+        </div>
       </ul>
     </nav>
   );
